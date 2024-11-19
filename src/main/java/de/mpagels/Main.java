6b8a88c4117abba791119346b265d40e3c116c54
mpagels <54703643+mpagels@users.noreply.github.com>
2024-11-19 14:28:28 +0100

    greet fans

--- src/main/java/de/mpagels/Main.java
+++ src/main/java/de/mpagels/Main.java
@@ -3,5 +3,10 @@ package de.mpagels;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hell Java Fans");
+    }
+
+    public static void greeting() {
+        System.out.println("hello people");
     }
 }
